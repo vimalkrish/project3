4b74ad165cbaaa806fd12885852b9330d912de3d
vimalkrish <linuxvimal@gmail.com>
2017-09-20 12:40:23 +0530

    test

--- main/main.tf
+++ main/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
   private_subnets = ["10.216.65.0/24", "10.216.67.0/24"]
   public_subnets  = ["10.216.64.0/24", "10.216.66.0/24"]
 
-  single_nat_gateway = "true"
+  #single_nat_gateway = "true"
   enable_nat_gateway = "true"
 
   azs      = ["us-east-1a", "us-east-1b"]
