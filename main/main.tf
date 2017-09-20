f3c997c171222ef81904e13faa04308174a941e9
root <root@ip-10-4-4-218.us-west-2.compute.internal>
2017-09-20 05:36:21 +0000

    test

---
+++ main/main.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+  source = "../module"
+
+  name = "Hubzu-Prod"
+
+  cidr = "10.216.64.0/19"
+  private_subnets = ["10.216.65.0/24", "10.216.67.0/24"]
+  public_subnets  = ["10.216.64.0/24", "10.216.66.0/24"]
+
+  single_nat_gateway = "true"
+  enable_nat_gateway = "true"
+
+  azs      = ["us-east-1a", "us-east-1b"]
+
+  private_subnet_tags = ["prod-private-primary-1a", "prod-private-secondary-1b"]
+  public_subnet_tags = ["prod-public-primary-1a", "prod-public-secondary-1b"]
+  
+  tags {
+    "Terraform" = "true"
+  }
+}
